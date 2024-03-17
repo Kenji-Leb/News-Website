5d99ab08b71927a7247e0fca4bcf9e3223685e1d
Abbas <45877262+Kenji-Leb@users.noreply.github.com>
2024-03-17 23:02:54 +0200

    testing db

--- Frontend/scripts/fetching.js
+++ Frontend/scripts/fetching.js
@@ -10,7 +10,7 @@ const getAllNews = () => {
       return response.json();
     })
     .then((data) => {
-      displayTodos(data);
+        displayNews(data);
     })
     .catch((error) => {
       console.error(error);
@@ -35,75 +35,4 @@ const displayNews = (data) => {
 }
 
 
-
-
-// const displayTodos = (data) => {
-//   todosDiv.innerHTML = "";
-//   data.todos?.forEach((todoItem) => {
-//     const todoDiv = document.createElement("div");
-
-//     const doneButton = document.createElement("button");
-//     doneButton.textContent = "Done";
-//     doneButton.addEventListener("click", () => toggleTodo(todoItem.id));
-
-//     const deleteButton = document.createElement("button");
-//     deleteButton.textContent = "Delete";
-//     deleteButton.addEventListener("click", () => deleteTodo(todoItem.id));
-
-//     const todoText = document.createElement("p");
-//     todoText.textContent = todoItem.text + " Done: " + todoItem.done;
-
-//     todoDiv.appendChild(todoText);
-//     todoDiv.appendChild(deleteButton);
-//     todoDiv.appendChild(doneButton);
-
-//     todosDiv.appendChild(todoDiv);
-//   });
-// };
-
-// const toggleTodo = async (id) => {
-//   try {
-//     const response = await fetch(`http://localhost/todo/api.php?id=${id}`, {
-//       method: "PUT",
-//     });
-//     const data = await response.json();
-//     if (data.status === "Success") {
-//       getAllTodos();
-//     } else {
-//       console.log("failed");
-//     }
-//   } catch (e) {
-//     console.error(error);
-//   }
-// };
-// const deleteTodo = (id) => {
-//   axios.delete(`http://localhost/todo/api.php?id=${id}`).then((response) => {
-//     if (response.data.status === "Success") {
-//       getAllTodos();
-//     } else {
-//       console.log("Failed");
-//     }
-//   });
-// };
-
-// const addTodo = async () => {
-//   try {
-//     const formData = new FormData();
-//     formData.append("text", todoInput.value);
-//     const response = await axios.post(
-//       `http://localhost/todo/api.php`,
-//       formData
-//     );
-//     todoInput.value = "";
-//     getAllTodos();
-//   } catch (e) {
-//     console.error(e);
-//   }
-// };
-
-// todoForm.addEventListener("submit", (e) => {
-//   e.preventDefault();
-//   addTodo();
-// });
-
-// getAllTodos();
+getAllNews()
